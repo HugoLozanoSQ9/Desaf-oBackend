16846b392c1643143717fc539044e427e2b1180c
HugoLozanoSQ9 <hugolozano19@aragon.unam.mx>
2024-07-05 15:50:27 -0600

    feat: was updated a getByid populate

--- src/usecases/post.usecase.js
+++ src/usecases/post.usecase.js
@@ -2,30 +2,30 @@ const Post = require('../models/post.model')
 
 const createError = require('http-errors')
 
-async function create(postData,userId) {
-    postData.user =userId
+async function create(postData, userId) {
+    postData.user = userId
     const newPost = await Post.create(postData)
     return newPost
 }
 
 async function getAll(search) {
     const allPosts = await Post.find().populate("user")
-    if(!search) return allPosts
-    const dataFiltered = allPosts.filter((elemento)=>elemento.title.includes(search))
+    if (!search) return allPosts
+    const dataFiltered = allPosts.filter((elemento) => elemento.title.includes(search))
     return dataFiltered
 }
 
-async function deleteById(id,userId) {
+async function deleteById(id, userId) {
 
     const postActual = await Post.findById(id)
 
-    if(!postActual) throw createError(404, "Post not found")
+    if (!postActual) throw createError(404, "Post not found")
 
     if (userId != postActual.user) {
         throw createError(401, "You cant delete this post, you not the owner")
     }
 
-    const postDeleted = await Post.findByIdAndDelete(id)  
+    const postDeleted = await Post.findByIdAndDelete(id)
 
     if (!postDeleted) throw createError(404, "Post not found")
 
@@ -33,12 +33,12 @@ async function deleteById(id,userId) {
 
 }
 
-async function updateByID(id, newPostData,userId) {
+async function updateByID(id, newPostData, userId) {
     newPostData.updatedAt = Date.now()
     const actualPost = await Post.findById(id)
-    if(actualPost.user != userId) throw createError(409, "you cant update a user")
+    if (actualPost.user != userId) throw createError(409, "you cant update a user")
 
-    const updatedPost= await Post.findByIdAndUpdate(id, newPostData, { new: true })
+    const updatedPost = await Post.findByIdAndUpdate(id, newPostData, { new: true })
     if (!updatedPost) throw createError(404, "Post not found")
 
     return updatedPost
@@ -47,9 +47,9 @@ async function updateByID(id, newPostData,userId) {
 
 async function getById(id) {
 
-    const postActual = await Post.findById(id)
+    const postActual = await Post.findById(id).populate("user")
 
-    if(!postActual) throw createError(404, "Post not found")
+    if (!postActual) throw createError(404, "Post not found")
 
     return postActual
 
